e108fd8653b5dc1ca5bfa30bcf8208796fab51f1
gautamN06 <nikeshgautam024@gmail.com>
2026-09-14 22:19:15 -0700

    Added Character model and API endpoints

--- backend/OnePieceApi/Program.cs
+++ backend/OnePieceApi/Program.cs
@@ -1,3 +1,5 @@
+using OnePieceApi.Models; 
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
@@ -5,27 +7,57 @@ builder.Services.AddOpenApi();
 var app = builder.Build();
 
 
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
 app.MapGet("/characters", () =>
 {
     return new[]
     {
-        new
+        new Character
         {
+            Id = 1,
             Name = "Monkey D. Luffy",
             Crew = "Straw Hat Pirates",
             Role = "Captain"
         },
-        new
+        new Character
         {
+            Id = 2,
             Name = "Roronoa Zoro",
             Crew = "Straw Hat Pirates",
             Role = "Swordsman"
         }
     };
 });
+
+
+app.MapGet("/characters/{id}", (int id) =>
+{
+    var characters = new []
+    {
+        new Character
+        {
+            Id = 1, 
+            Name = "Monkey D. Luffy",
+            Crew = "Straw Hat Pirates",
+            Role = "Captain"
+        },
+
+        new Character
+        {
+            Id = 2, 
+            Name = "Roronoa Zoro",
+            Crew = "Straw Hat Pirates",
+            Role = "Swordsman"
+        }
+    };
+
+    var character = characters.FirstOrDefault(c=>c.Id == id);
+
+    if (character == null)
+    {
+        return Results.NotFound();
+    } 
+
+    return Results.Ok(character);
+});
+
 app.Run();
---
+++ backend/OnePieceApi/models/Character.cs
@@ -0,0 +1,9 @@
+namespace OnePieceApi.Models;
+
+public class Character
+{
+    public int Id { get; set;}
+    public string Name { get; set; } = ""; 
+    public string Crew { get; set; } = "";
+    public string Role { get; set; } = "";
+}
